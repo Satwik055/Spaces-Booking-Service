b4e064e217ffb071afcd380b8fa4afd9c816179e
7week <satwikkumar055@gmail.com>
2024-08-31 01:39:56 +0530

    added 2 endpoints

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify, request
 import psycopg2
 
-
 DB_CONFIG = {
     'host': "aws-0-ap-south-1.pooler.supabase.com",
     'database': "postgres",
@@ -13,11 +12,48 @@ connection = psycopg2.connect(**DB_CONFIG)
 cursor = connection.cursor()
 app = Flask(__name__)
 
+
+@app.route('/bookings/<int:user_id>', methods=['GET'])
+def get_all_bookings(user_id):
+    try:
+
+        cursor.callproc('get_all_bookings', [user_id])
+        response = cursor.fetchone()
+
+        if response is None:
+            raise Exception(f"No booking available of {user_id} type")
+
+        else:
+            columns = [desc[0] for desc in cursor.description]
+            response_dict = dict(zip(columns, response))
+            properties = [response_dict]
+            return jsonify(properties)
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route('/booking/<int:booking_id>', methods=['GET'])
+def get_booking_by_id(booking_id):
+    try:
+        cursor.callproc('get_booking_by_id', [booking_id])
+
+        response = cursor.fetchone()
+
+        if response is None:
+            return jsonify({"error": f"No booking with id {booking_id} found"}), 400
+        else:
+            columns = [desc[0] for desc in cursor.description]
+            response_dict = dict(zip(columns, response))
+            return jsonify(response_dict)
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
 @app.route('/hello', methods=['GET'])
 def hello():
-    return "Holla from booking service"
-
-
+    return "Hi from booking service"
 
 
 if __name__ == '__main__':
